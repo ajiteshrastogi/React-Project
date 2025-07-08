e40fe81564d747dc0df3bcec0e2deffa54fa5d18
ajiteshrastogi <ajiteshrastogi19@gmail.com>
2025-07-08 18:34:53 +0530

    RTK and Tailwind is ingtrated, store and Slices are made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authservice from './appwrite/auth'
+import { login, logout } from './app/authSlice';
+import { Header, Footer } from './components';
 
 function App() {
+  const [loading , setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>Let's Make a project</h1>
-    </>
-  )
+  useEffect(()=>{
+    authservice.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}));
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false));
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO: {/*  <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
